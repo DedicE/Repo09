17e30c1b253a398ee5bb1f7b06a7fa16086abbb1
Emir Dedic <dedic.emir17@gmail.com>
2022-04-06 02:09:18 +0200

    If/else

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,7 +1,10 @@
 function isLess(a, b) {
-    return a < b;
+    if (a < b){
+    return true;
+  } else {
+    return false;
+    }
 }
-
 console.log(isLess(10, 15));
 console.log(isLess(15, 10));
 

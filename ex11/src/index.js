454b68535310b4af054e41f1ec9b025bf340b7ff
Emir Dedic <dedic.emir17@gmail.com>
2022-04-09 02:02:36 +0200

    Update index.js

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,9 +1,9 @@
 function functionElse(num) {
     var result;
     if(num <= 5) {
-        result = "5 or smaller";
+    var result = "5 or smaller";
     } else { 
-        result = "Bigger than 5";
+    var result = "Bigger than 5";
     }
     return result;
 }
